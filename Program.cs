bc9d1510c22543894cc3c3feb96f1bdf672b5759
Erhan Topcu <erhn.topcu@gmail.com>
2022-06-23 22:04:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedi", "Köpek","Kuş","Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+            
+            //Dİzilere deger atama vee erişim
+
+            renkler[0] ="Mavi";
+            dizi[3]=10;
+            
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngüler ile dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını alan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayisini giriniz ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+                
+            }
+            int toplam=0;
+            foreach (var sayi in sayiDizisi)
+            
+                toplam += sayi;
+                Console.WriteLine("Ortalama :" + toplam/diziUzunlugu);
+            
+        }
+    }
+}
